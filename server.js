7d23e40a3421301b173611979a658918bc85c1d1
Lukas Klappert <lklappert.mmt-b2013@fh-salzburg.ac.at>
2015-05-05 20:21:32 +0200

    start listening in server

--- server.js
+++ server.js
@@ -1,4 +1,24 @@
+var dgram = require('dgram');
+
 var Server = function () {
+
+    this.socket = dgram.createSocket('udp4');
+    this.port = 3000;
+    this.host = '127.0.0.1';
+
+    var that = this;
+
+    this.socket.on('listening', function() {
+        var address = that.socket.address();
+        console.log('UDP Server listening on ' + address.address + ':' + address.port);
+    });
+
+    this.socket.on('message', function(msg) {
+        console.log(msg);
+        that.socket.close();
+    })
+
+    this.socket.bind(this.port, this.host);
 
 }
 
